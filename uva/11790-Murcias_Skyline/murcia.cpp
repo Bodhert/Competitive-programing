077b11eee830b2d9f8e1e919418fc19511cb290e
Bodhert <alejandro.cordoba.bodhert@gmail.com>
2017-01-23 19:50:56 -0500

    working

--- uva/11790-Murcias_Skyline/murcia.cpp
+++ uva/11790-Murcias_Skyline/murcia.cpp
@@ -26,15 +26,16 @@ int LIS(vector<int>& h, vector<int>& w ){
   
 }
 
-int LDS(vector<int>& h, vector<int>& w){ // formularme mejor este metodo debido a que esta malo
+int LDS(vector<int>& h, vector<int>& w){ 
   int size = h.size();
   int L[size], L_id[size], P[size];
 
   int lis = 0, lis_end = 0;
   for(int i = 0; i < size; ++i){
-    int pos = upper_bound(L, L + lis, h[i]) - L;
-    L[pos] = h[i];
-    L_id[pos] = i;
+    int temp = upper_bound(L, L + lis, h[i]) - L; 
+    int pos = temp ? temp - 1: lis; 
+    L[pos] = h[i]; 
+    L_id[pos] = i; 
     P[i] = pos ? L_id[pos - 1] : -1;
     if(pos + 1 >= lis){ 
       lis = pos + 1;
@@ -45,9 +46,8 @@ int LDS(vector<int>& h, vector<int>& w){ // formularme mejor este metodo debido 
   int x = lis_end, sum = 0;
   for(;P[x] >= 0; x = P[x]) sum += w[x];
   sum += w[x];
-
-  return sum;
   
+  return sum;
   
 }
 
@@ -86,3 +86,10 @@ int main(){
   }
   return 0;
 }
+
+
+//revisar el ultimo caso
+/*
+  aparentemente ya me esta sacando los lds y lis correspondiente, lo que debo de tener en cuenta
+  es el criterio de desempate, en este caso es el peso o la suma del ancho de los edificios
+ */
