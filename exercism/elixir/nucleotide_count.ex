8df3ca06957aad184e32417b0581a4eaec745402
Alejandro Cordoba Boder <acordobab@acordobab-4.local>
2020-09-20 18:53:55 -0500

    another ez one

---
+++ exercism/elixir/nucleotide_count.ex
@@ -0,0 +1,35 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+  @map %{?A => 0, ?C => 0 , ?T => 0 , ?G => 0}
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count(charlist(), char()) :: non_neg_integer()
+  def count(strand, nucleotide) do
+    Enum.count(strand, fn st -> st == nucleotide end)
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram(charlist()) :: map()
+  def histogram(strand) do
+    strand |>
+    Enum.reduce(@map, fn st, acc ->
+       Map.put(acc, st, (acc[st] || 0) + 1)
+      end)
+  end
+end
