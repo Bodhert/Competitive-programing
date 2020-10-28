bf4052c3edba5aa924df4004a7a92b0dbdb303e8
Alejandro Cordoba Boder <acordobab@acordobab-4.local>
2020-10-28 18:18:37 -0500

    another one

---
+++ exercism/elixir/string_series.ex
@@ -0,0 +1,19 @@
+defmodule StringSeries do
+  @doc """
+  Given a string `s` and a positive integer `size`, return all substrings
+  of that size. If `size` is greater than the length of `s`, or less than 1,
+  return an empty list.
+  """
+  @spec slices(s :: String.t(), size :: integer) :: list(String.t())
+  def slices(s, size) do
+    if String.length(s) < size or size <= 0 do
+      []
+    else
+      for pos <- 0..String.length(s)-size do
+        String.slice(s,pos..pos+size-1)
+      end
+    end
+  end
+
+
+end
