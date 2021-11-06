f6da9e79c1b5137f8890b060b9ec5be63edc7fd4
bodhert <alejandro.cordoba.bodhert@gmail.com>
2021-11-05 20:27:07 -0500

    e-z exercise for fun

---
+++ exercism/elixir/lasagna.ex
@@ -0,0 +1,27 @@
+defmodule Lasagna do
+  @spec expected_minutes_in_oven :: 40
+  def expected_minutes_in_oven do
+    40
+  end
+
+  @spec remaining_minutes_in_oven(integer) :: integer
+  def remaining_minutes_in_oven(remaining_minutes) do
+    expected_minutes_in_oven() - remaining_minutes
+  end
+
+  @spec preparation_time_in_minutes(integer) :: integer
+  def preparation_time_in_minutes(layers) do
+    layers * 2
+  end
+
+  @spec total_time_in_minutes(integer, integer) :: integer
+  def total_time_in_minutes(layers, minutes) do
+    preparation_time_in_minutes(layers) + minutes
+  end
+
+  @spec alarm :: String.t
+  def alarm do
+    "Ding!"
+  end
+
+end
